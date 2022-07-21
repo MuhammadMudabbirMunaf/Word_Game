5fce5e137f76ec614688c3a20ea446ef785a7f21
Muhammad Mudabbir <106159197+MuhammadMudabbirMunaf@users.noreply.github.com>
2022-07-21 16:52:12 +0500

    Add files via upload

---
+++ word_game/app.js
@@ -0,0 +1,26 @@
+// let input = document.getElementById("input");
+// buttons = document.querySelectorAll("button");
+// let inputValue = "";
+// for (item of buttons) {
+//     item.addEventListener("click", (e) => {
+//         buttonText
+//     }
+//     )
+// }
+
+
+// function myFunction() {
+//     document.getElementById("a").innerHTML = "A";
+//   }
+
+function a(args) {
+    if(args === "1"){
+        document.getElementById("word").innerText += "A";
+    }
+    else if(args === "2"){
+        document.getElementById("word").innerText += "B";
+    }
+    else if(args === "3"){
+        document.getElementById("word").innerText += "C";
+    }
+}
